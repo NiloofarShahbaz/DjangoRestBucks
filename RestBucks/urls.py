a7827c16719cace996289566bb500835d691d1e3
niloofar.shahbaz <niloofar.shahbaz@gmail.com>
2021-08-23 11:25:43 +0430

    :sparkles: Add product list view

--- RestBucks/urls.py
+++ RestBucks/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+from product.urls import urlpatterns as product_urls
+
+api_urls = product_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(api_urls)),
 ]
